ab167f2877b4762575a6f151abf12960b700ede4
kirito <dashakukuk@gmail.com>
2022-12-06 18:35:58 +0300

    Создан сайт

---
+++ src/main/java/com/example/MyWebProjectt/MyWebProjecttApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyWebProjectt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebProjecttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebProjecttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyWebProjectt/MyWebProjecttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyWebProjectt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebProjecttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
